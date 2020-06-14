4a34d07ba238ce68566405acab67b73bbcfbda03
yu.xia <404638507@qq.com>
2020-06-14 19:43:05 +0800

    problems-739

--- src/com/lilyandlucy/algorithm/DailyTemperatures.java
+++ src/com/lilyandlucy/algorithm/DailyTemperatures.java
@@ -1,6 +1,9 @@
 package com.lilyandlucy.algorithm;
 
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * 请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
  * <p>
@@ -14,5 +17,23 @@ package com.lilyandlucy.algorithm;
  */
 
 public class DailyTemperatures {
+
+    public static void main(String[] args) {
+        int[] input = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dailyTemperatures(input)));
+    }
+
+    public static int[] dailyTemperatures(int[] T) {
+        int[] result = new int[T.length];
+        Stack<Integer> stack = new Stack();
+        for (int i = 0; i < T.length; i++) {
+            while (!stack.isEmpty() && T[i] >= T[stack.peek()]) {
+                int prevIndex = stack.pop();
+                result[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
 
 }
