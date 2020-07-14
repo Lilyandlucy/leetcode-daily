24434e731de0481f142d8104172d8df26beed87a
yu.xia <404638507@qq.com>
2020-07-14 22:26:04 +0800

    problems-120

---
+++ src/com/lilyandlucy/algorithm/MinimumTotal.java
@@ -0,0 +1,55 @@
+package com.lilyandlucy.algorithm;
+
+import java.util.List;
+
+/**
+ * 120. 三角形最小路径和
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * <p>
+ * 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ * <p>
+ * <p>
+ * <p>
+ * 例如，给定三角形：
+ * <p>
+ * [
+ * [2],
+ * [3,4],
+ * [6,5,7],
+ * [4,1,8,3]
+ * ]
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ * <p>
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
+ */
+public class MinimumTotal {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
+    }
+
+    public int minimumTotalV2(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+
+}
